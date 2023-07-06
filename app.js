30ae2a9954278a6a0aae1fd926a4b10d2bf18a2c
Mfon Itohowo <mfonation29@gmail.com>
2023-07-06 11:46:33 +0100

    Final commit

--- app.js
+++ app.js
@@ -8,58 +8,69 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
-const navToggle = document.querySelector(".nav-toggle")
-const linksContainer = document.querySelector(".links-container")
-const links = document.querySelector(".links")
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function() {
     //linksContainer.classList.toggle("show-links")
-    const containerHeight = linksContainer.getBoundingClientRect().height
-    const linksHeight = links.getBoundingClientRect().height
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
 
     if(containerHeight === 0) {
-        linksContainer.style.height = `${linksHeight}px`
+        linksContainer.style.height = `${linksHeight}px`;
     } else{
-        linksContainer.style.height = 0
-    }
-})
-const navbar = document.getElementById("nav")
-const topLink = document.querySelector(".top-link")
+        linksContainer.style.height = 0;
+    };
+});
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 // ********** fixed navbar ************
 window.addEventListener("scroll", function() {
     const scrollHeight = window.scrollY;
-    const navHeight = navbar.getBoundingClientRect().height
+    const navHeight = navbar.getBoundingClientRect().height;
 
     if(scrollHeight > navHeight) {
-        navbar.classList.add("fixed-nav")
+        navbar.classList.add("fixed-nav");
     } else {
-        navbar.classList.remove("fixed-nav")
-    }
+        navbar.classList.remove("fixed-nav");
+    };
 
     if(scrollHeight > 500) {
-        topLink.classList.add("show-link")
+        topLink.classList.add("show-link");
     } else {
-        topLink.classList.remove("show-link")
-    }
-})
+        topLink.classList.remove("show-link");
+    };
+});
 
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll(".scroll-link")
+const scrollLinks = document.querySelectorAll(".scroll-link");
 
 scrollLinks.forEach(function(link) {
     link.addEventListener("click", function(e) {
         // prevent default
-        e.preventDefault()
+        e.preventDefault();
         // navigate to specific spot
-        const id = e.currentTarget.getAttribute("href").slice(1)
-        //console.log(id);
-        const element = document.getElementById(id)
-        let position = element.offsetTop
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav) {
+            position = position - navHeight;
+        };
+        if(navHeight > 82) {
+            position = position + containerHeight;
+        };
+
         window.scrollTo({
             left: 0, 
             top: position,
-        })
+        });
         linksContainer.style.height = 0;
-    })
-})
+    });
+});
